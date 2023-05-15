2dbe3e1ecb56b767c9e7ae6c3d95ccaf53a6a437
Bharath A <89858675+bharathkadur@users.noreply.github.com>
2023-05-15 23:31:13 +0530

    Update absoluteValuesSumMinimization.py

--- The Core/32. absoluteValuesSumMinimization/absoluteValuesSumMinimization.py
+++ The Core/32. absoluteValuesSumMinimization/absoluteValuesSumMinimization.py
@@ -3,3 +3,18 @@ def solution(a):
     if i % 2 == 0 or len(a) == 2:
         return a[i - 1]
     return a[i]
+
+
+
+
+def solution(a):
+    min_number = 0
+    min_sum = 2147483648
+    for i in a:
+        Sum = 0
+        for j in a:
+            Sum += abs(j-i)
+        if Sum < min_sum:
+            min_sum = Sum
+            min_number = i
+    return min_number
