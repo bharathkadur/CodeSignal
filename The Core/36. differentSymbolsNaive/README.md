fcb53285228cbadde57a9fe6d67a0d513232b5c0
Bharath A <89858675+bharathkadur@users.noreply.github.com>
2023-05-17 00:42:00 +0530

    Delete The Core/36. differentSymbolsNaive directory

--- The Core/36. differentSymbolsNaive/README.md
+++
@@ -1,4 +0,0 @@
-<h1>differentSymbolsNaive
-</h1>
-<p>Given a string, find the number of different characters in it.
- </p>
--- The Core/36. differentSymbolsNaive/differentSymbolNaive.py
+++
@@ -1,8 +0,0 @@
-def solution(s):
-    a = []
-    count = 0
-    for i in s:
-        if i not in a:
-            count = count + 1
-            a.append(i)      
-    return count
