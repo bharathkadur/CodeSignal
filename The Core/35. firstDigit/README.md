04dfe51d78b743b257a1b178923d0f6d19e441fc
Bharath A <89858675+bharathkadur@users.noreply.github.com>
2023-05-17 00:41:46 +0530

    Delete The Core/35. firstDigit directory

--- The Core/35. firstDigit/README.md
+++
@@ -1,4 +0,0 @@
-<h1>firstDigit
-</h1>
-<p>Find the leftmost digit that occurs in a given string.
- </p>
--- The Core/35. firstDigit/firstDigit.py
+++
@@ -1,5 +0,0 @@
-def solution(inputString):
-    for i in inputString:
-        if i.isdigit():
-            return i
-
