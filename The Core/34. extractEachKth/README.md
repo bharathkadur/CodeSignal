a0747d9fff0557ae941113f833006266b350db7e
Bharath A <89858675+bharathkadur@users.noreply.github.com>
2023-05-17 00:41:37 +0530

    Delete The Core/34. extractEachKth directory

--- The Core/34. extractEachKth/README.md
+++
@@ -1,4 +0,0 @@
-<h1>extractEachKth
-</h1>
-<p>Given array of integers, remove each kth element from it.
- </p>
--- The Core/34. extractEachKth/extractEachKth.py
+++
@@ -1,6 +0,0 @@
-def solution(inputArray, k):
-    result = []
-    for i in range(len(inputArray)):
-        if (i + 1) % k != 0:
-            result.append(inputArray[i])
-    return result
