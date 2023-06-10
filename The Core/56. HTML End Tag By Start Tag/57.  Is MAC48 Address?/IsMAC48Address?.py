0762020cec36c906e69f6c472f220f0f513a6f21
Bharath A <89858675+bharathkadur@users.noreply.github.com>
2023-06-10 19:44:01 +0530

    Delete The Core/56. HTML End Tag By Start Tag/57.  Is MAC48 Address? directory

--- The Core/56. HTML End Tag By Start Tag/57.  Is MAC48 Address?/IsMAC48Address?.py
+++
@@ -1,8 +0,0 @@
-import re
-def solution(inputString):
-    pattern = r'^([0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}$'
-    match = re.match(pattern, inputString)
-    if match:
-        return True
-    else:
-        return False
